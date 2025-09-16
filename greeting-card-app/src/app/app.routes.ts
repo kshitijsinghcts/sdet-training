fa74945439e06ce931785377baa0cd69be1573de
Singh, Kshitij (Cognizant) <2438044@cognizant.com>
2025-09-16 14:17:21 +0530

    Fixed routes

--- greeting-card-app/src/app/app.routes.ts
+++ greeting-card-app/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { GreetingCardComponent } from './greeting-card/greeting-card';
+
+export const routes: Routes = [
+  {
+    path: 'greeting',
+    component: GreetingCardComponent
+  }
+];
